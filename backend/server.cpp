f975f70907f2295fcee87e538fde2a3d5ba70b61
Nicolás <148826576+NicoEve@users.noreply.github.com>
2026-09-02 17:09:43 -0500

    Add files via upload

---
+++ backend/server.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+#define PORT 5000
+
+int clientSocket = -1;
+
+// void* sendPosition(void* arg) {}
+
+int main() {
+
+}
